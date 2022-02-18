c2c4b20013abdb55b1513888b04fd2f2c5d20454
Alcibiades Athens <alcibiades.eth@protonmail.com>
2022-02-18 12:36:49 -0700

    character framework

--- src/Avatar.sol
+++ src/Avatar.sol
@@ -1,35 +1,65 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: BUSL 1.1
 pragma solidity 0.8.10;
 
 import "solmate/tokens/ERC721.sol";
 
-contract Avatar is ERC721 {
+contract Avatar is ERC721, ERC721TokenReceiver {
 
-    constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol) {}
+    function onERC721Received(
+        address _operator,
+        address _from,
+        uint256 _id,
+        bytes calldata _data
+    ) public virtual override returns (bytes4) {
+        return ERC721TokenReceiver.onERC721Received.selector;
+    }
 
-    function tokenURI(uint256) public pure virtual override returns (string memory) {
+    // TODO(Later, these characters can be deposited for a v1 character)
+    struct AvatarSheet {
+        uint32 experience;
+        uint16 hp;
+        uint16 dp;
+        uint16 ap;
+    }
+
+    address public loot;
+
+    uint256 _next_id = 0;
+
+    // Name for character
+    mapping(uint256 => string) public avatarName;
+
+    // Mapping to get stats of
+    mapping(uint256 => AvatarSheet) public stats;
+
+    constructor(address _loot) ERC721("ROMAvatar", "ROMA") {
+        loot = _loot;
+    }
+
+    function tokenURI(uint256 id) public view virtual override returns (string memory) {
+        require(id < _next_id, "Avatar not yet minted");
+        // TODO(Return image)
         string memory uri = "";
         return uri;
     }
 
-    function mint(address to, uint256 tokenId) public virtual {
-        _mint(to, tokenId);
-    }
-
-    function burn(uint256 tokenId) public virtual {
-        _burn(tokenId);
-    }
-
-    function safeMint(address to, uint256 tokenId) public virtual {
+    function mint(address to, string memory newAvatarName) public virtual {
+        // TODO(Charge mint cost here)
+        // Get the next token id
+        uint256 tokenId = _next_id;
+        // Mint the NFT
         _safeMint(to, tokenId);
+        // Set Avatar data
+        avatarName[tokenId] = newAvatarName;
+        AvatarSheet memory sheet;
+        sheet.hp = 5;
+        sheet.ap = 1;
+        sheet.dp = 1;
+        stats[tokenId] = sheet;
+        // Increment ID
+        _next_id += 1;
     }
 
-    function safeMint(
-        address to,
-        uint256 tokenId,
-        bytes memory data
-    ) public virtual {
-        _safeMint(to, tokenId, data);
-    }
+    // TODO(Equipment/loot system)
 
 }
--- src/test/Avatar.t.sol
+++ src/test/Avatar.t.sol
@@ -3,11 +3,30 @@ pragma solidity 0.8.10;
 
 import "../Avatar.sol";
 import "ds-test/test.sol";
+import "solmate/tokens/ERC721.sol";
 
-contract AvatarTest is DSTest {
-    function setUp() public {}
+contract ERC721Recipient is ERC721TokenReceiver {
 
-    function testExample() public {
-        assertTrue(true);
+    function onERC721Received(
+        address _operator,
+        address _from,
+        uint256 _id,
+        bytes calldata _data
+    ) public virtual override returns (bytes4) {
+        return ERC721TokenReceiver.onERC721Received.selector;
     }
+}
+
+contract AvatarTest is DSTest, ERC721Recipient {
+    Avatar avatar;
+
+    function setUp() public {
+        // TODO(Deploy and integrate loot here)
+        avatar = new Avatar(address(0));
+    }
+
+    function testMint() public {
+        avatar.mint(address(this), "Uriel");
+    }
+
 }
